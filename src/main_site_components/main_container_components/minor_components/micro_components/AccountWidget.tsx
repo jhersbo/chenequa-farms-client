2bd5d7a7937f7a557372dfed768c16e11a3d2022
jhersbo <jhersbo@gmail.com>
2022-11-16 12:31:11 -0600

    can now reset pass from client

--- src/main_site_components/main_container_components/minor_components/micro_components/AccountWidget.tsx
+++ src/main_site_components/main_container_components/minor_components/micro_components/AccountWidget.tsx
@@ -6,6 +6,24 @@ import { serverURL } from "../../../../App"
 import Cookies from "js-cookie"
 
 import TextField from '@mui/material/TextField';
+import CloseIcon from '@mui/icons-material/Close';
+import { Bars } from "react-loader-spinner"
+
+export const textFieldSXProps = {
+    marginBottom: "0.5rem",
+    width: "100%"
+}
+
+export const closeIconSXProps = {
+    fontSize: "28px"
+}
+
+export const loadingBarsStyle = {
+    height: "40",
+    width: "40",
+    color: "#005B31",
+    ariaLabel: "bars-loading"
+}
 
 interface AccountWidgetProps{
     setUser: React.Dispatch<React.SetStateAction<UserContextInterface | null>>,
@@ -17,8 +35,6 @@ interface AccountWidgetProps{
 }
 
 const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegState, setBlur }: AccountWidgetProps)=>{
-
-    const [ validationError, setValidationError ] = useState({state: false, message: ""})
     
     const [ emailInput, setEmailInput ] = useState("")
     const [ firstName, setFirstName ] = useState("")
@@ -26,16 +42,17 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     const [ password_1, setPassword_1 ] = useState("")
     const [ password_2, setPassword_2 ] = useState("")
     const [ forgotPassword, setForgotPassword ] = useState(false)
+    const [ recoverySuccess, setRecoverySuccess ] = useState(false)
+    const [ validationError, setValidationError ] = useState({state: false, message: ""})
 
-    const textFieldSXProps = {
-        marginBottom: "0.5rem",
-        width: "100%"
-    }
-
+    const [ isLoading, setIsLoading ] = useState(false)
+    
     const user = useContext(UserContext)
 
     const handleSignOut = ()=>{
         setUser(null)
+        setEmailInput("")
+        setPassword_1("")
         Cookies.set("user", "")
     }
 
@@ -50,6 +67,8 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     }
 
     const handleLoginSubmit = async ()=>{
+
+        setIsLoading(true)
 
         let response = await fetch(serverURL + "user_auth/auth",{
             method: "POST",
@@ -66,12 +85,55 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
         console.log(parsedResponse)
 
         if(!parsedResponse.success){
+            setIsLoading(false)
             setValidationError({state: true, message: "Unable to log in. Please check your login credentials."})
             return
         }
 
         setUser(parsedResponse.data)
         Cookies.set("user", JSON.stringify(parsedResponse.data))
+        setIsLoading(false)
+        setLoginState(false)
+        setRegState(false)
+        setBlur(false)
+        return
+    }
+
+    const handleRegistration = async ()=>{
+
+        setIsLoading(true)
+
+        let passwordsMatch = password_1 === password_2;
+
+        if(!passwordsMatch){
+            setValidationError({state: true, message: "Passwords do not match"})
+            return
+        }
+
+        let response = await fetch(serverURL + "user_auth", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email_address: emailInput,
+                first_name: firstName,
+                last_name: lastName,
+                password_hash: password_1
+            })
+        })
+
+        let parsedResponse = await response.json()
+
+        if(!parsedResponse.success){
+            setIsLoading(false)
+            setValidationError({state: true, message: "Unable to create account. Please check your credentials."})
+            return
+        }
+
+        setUser(parsedResponse.data)
+        Cookies.set("user", JSON.stringify(parsedResponse.data))
+        setIsLoading(false)
         setLoginState(false)
         setRegState(false)
         setBlur(false)
@@ -83,11 +145,13 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     }
 
     const updateFirstNameInput = (event: any)=>{
-
+        setValidationError({state: false, message: ""})
+        setFirstName(event.target.value)
     }
 
     const updateLastNameInput = (event: any)=>{
-        
+        setValidationError({state: false, message: ""})
+        setLastName(event.target.value)
     }
 
     const updatePassword_1Input = (event: any)=>{
@@ -101,17 +165,76 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     }
 
     const handleForgotPasswordState = ()=>{
+        setValidationError({state: false, message: ""})
         setLoginState(false)
         setRegState(false)
         setForgotPassword(true)
     }
 
+    const handleSendRecoveryEmail = async ()=>{
+        setIsLoading(true)
 
+        if(emailInput === ""){
+            setIsLoading(false)
+            setValidationError({state: true, message: "Your email address is required."})
+            return
+        }
+
+        let response = await fetch(serverURL + "user_auth/forgot-password", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email_address: emailInput
+            })
+        })
+
+        let parsedResponse = await response.json()
+
+        if(!parsedResponse.success){
+            setIsLoading(false)
+            setValidationError({state: true, message: "Unable to send password reset email. Please check to be sure your email address is correct."})
+            return
+        }
+
+        setRecoverySuccess(true)
+        setIsLoading(false)
+        setLoginState(false)
+        setRegState(false)
+        return
+    }
+
+    const handleCloseBtn = ()=>{
+        setForgotPassword(false)
+        setRecoverySuccess(false)
+        setIsLoading(false)
+        setLoginState(false)
+        setRegState(false)
+        setBlur(false)
+    }
 
     if(loginState && !regState){
         return(
             <div id="login-container">
-                <h3>Login</h3>
+                <button 
+                    className="close-btn"
+                    onClick={()=>{handleCloseBtn()}}>
+                    <CloseIcon sx={closeIconSXProps}/>
+                </button>
+                <header id="login-reg-header">
+                    <h3>Login</h3>
+                    {
+                        isLoading ?
+                            <Bars
+                                height={loadingBarsStyle.height}
+                                width={loadingBarsStyle.width}
+                                color={loadingBarsStyle.color}
+                            />
+                        :
+                            null
+                    }
+                </header>
                 <div id="login-form-container">
                     <TextField
                         id="email-input"
@@ -135,16 +258,27 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
                             null
                     }
                     <div id="login-btn-container">
-                        <button onClick={()=>{handleLoginSubmit()}}>Login</button>
+                        <button 
+                            className="widget-btn login-reg-submit" 
+                            onClick={()=>{handleLoginSubmit()}}
+                            disabled={isLoading ? true : false}>
+                                Login
+                        </button>
                         {
                             validationError.state ?
-                                <button onClick={()=>{handleForgotPasswordState()}}>
+                                <button className="widget-btn" onClick={()=>{handleForgotPasswordState()}}
+                                disabled={isLoading ? true : false}>
                                     Forgot your password?
                                 </button>
                             :
                                 null
                         }
-                        <button onClick={()=>{handleSwitchToReg()}}>Register</button>
+                        <button 
+                            className="widget-btn login-reg-submit" 
+                            onClick={()=>{handleSwitchToReg()}}
+                            disabled={isLoading ? true : false}>
+                                Register
+                        </button>
                     </div>
                 </div>
             </div>
@@ -152,7 +286,24 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     }else if(!loginState && regState){
         return(
             <div id="login-container">
-                <h3>Register</h3>
+                <button 
+                    className="close-btn"
+                    onClick={()=>{handleCloseBtn()}}>
+                    <CloseIcon sx={closeIconSXProps}/>
+                </button>
+                <header id="login-reg-header">
+                    <h3>Register</h3>
+                    {
+                        isLoading ?
+                            <Bars
+                                height={loadingBarsStyle.height}
+                                width={loadingBarsStyle.width}
+                                color={loadingBarsStyle.color}
+                            />
+                        :
+                            null
+                    }
+                </header>
                 <div id="login-form-container">
                     <TextField
                         id="email-input"
@@ -189,9 +340,27 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
                         onChange={(e)=>{updatePassword_2Input(e)}}
                         sx={textFieldSXProps}
                     />
+                    {
+                        validationError.state ? 
+                            <h5>
+                                {validationError.message}
+                            </h5>
+                        :
+                            null
+                    }
                     <div id="login-btn-container">
-                        <button>Register</button>
-                        <button onClick={()=>{handleSwitchToLogin()}}>Have an account?</button>
+                        <button 
+                            className="widget-btn login-reg-submit"
+                            onClick={()=>{handleRegistration()}}
+                            disabled={isLoading ? true : false}>
+                                Register
+                        </button>
+                        <button 
+                            className="widget-btn login-reg-submit" 
+                            onClick={()=>{handleSwitchToLogin()}}
+                            disabled={isLoading ? true : false}>
+                                Have an account?
+                        </button>
                     </div>
                 </div>
             </div>
@@ -199,9 +368,52 @@ const AccountWidget = ({ setUser, loginState, setLoginState, regState, setRegSta
     }else if(forgotPassword){
         return(
             <div id="login-container">
-                <h3>
-                    Forgot your password?
-                </h3>
+                <button 
+                    className="close-btn"
+                    onClick={()=>{handleCloseBtn()}}>
+                    <CloseIcon sx={closeIconSXProps}/>
+                </button>
+                <header id="login-reg-header">
+                    <h3>Forgot your password?</h3>
+                    {
+                        isLoading ?
+                            <Bars
+                                height={loadingBarsStyle.height}
+                                width={loadingBarsStyle.width}
+                                color={loadingBarsStyle.color}
+                            />
+                        :
+                            null
+                    }
+                </header>
+                <h5>Enter your email below and you will receive an email to reset your password.</h5>
+                <TextField
+                    id="email-input"
+                    label="Email address"
+                    onChange={(e)=>{updateEmailInput(e)}}
+                    sx={textFieldSXProps}
+                />
+                {
+                    validationError.state ? 
+                        <h5>
+                            {validationError.message}
+                        </h5>
+                    :
+                        null
+                }
+                <div id="login-btn-container">
+                    <button 
+                        className="widget-btn login-reg-submit"
+                        disabled={isLoading ? true : false}
+                        onClick={()=>{handleSendRecoveryEmail()}}>
+                            {
+                                recoverySuccess ? 
+                                    "Email Sent!"
+                                :
+                                    "Send Email"
+                            }
+                    </button>
+                </div>
             </div>
         )
     }else{
--- src/side_components/ResetPassword.tsx
+++ src/side_components/ResetPassword.tsx
@@ -1,8 +1,148 @@
+import "./Sass/ResetPassword.scss"
+import "../main_site_components/main_container_components/minor_components/micro_components/Sass/AccountWidget.scss"
+
+import { TextField } from "@mui/material"
+import { Bars } from "react-loader-spinner"
+
+import { textFieldSXProps, loadingBarsStyle } from "../main_site_components/main_container_components/minor_components/micro_components/AccountWidget"
+import { serverURL } from "../App"
+import { useState } from "react"
+
 const ResetPassword = ()=>{
+
+    const [ emailInput, setEmailInput ] = useState("")
+    const [ password_1, setPassword_1 ] = useState("")
+    const [ password_2, setPassword_2 ] = useState("")
+    const [ isLoading, setIsLoading ] = useState(false)
+    const [ recoverySuccess, setRecoverySuccess ] = useState(false)
+    const [ validationError, setValidationError ] = useState({state: false, message: ""})
+
+    //starts slicing after '/forgot-password/'
+    const parsedToken = window.location.pathname.slice(17)
+    console.log(parsedToken)
+
+    const updateEmailInput = (event: any)=>{
+        setEmailInput(event.target.value)
+    }
+
+    const updatePassword_1Input = (event: any)=>{
+        setValidationError({state: false, message: ""})
+        setPassword_1(event.target.value)
+    }
+
+    const updatePassword_2Input = (event: any)=>{
+        setValidationError({state: false, message: ""})
+        setPassword_2(event.target.value)
+    }
+
+    const submitPasswordUpdate = async ()=>{
+        setIsLoading(true)
+        let passwordsMatch = password_1 === password_2;
+
+        if(!passwordsMatch){
+            setIsLoading(false)
+            setValidationError({state: true, message: "Passwords do not match."})
+            return
+        }
+
+        let response = await fetch(serverURL + "user_auth/forgot-password", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                email_address: emailInput,
+                password_hash: password_1,
+                reset_password_token: parsedToken
+            })
+        })
+
+        let parsedResponse = await response.json()
+        console.log(parsedResponse)
+
+        if(!parsedResponse.success){
+            setIsLoading(false)
+            setValidationError({state: true, message: "Password update failed. Please request another reset email."})
+            return
+        }
+        setRecoverySuccess(true)
+        setIsLoading(false)
+        return
+    }
+
     return(
-        <>
-            Reset password
-        </>
+        <div id="reset-container">
+            <div id="reset-form">
+                <header id="reset-header">
+                    <h3>Reset your password</h3>
+                    {
+                        recoverySuccess ?
+                            <h5 id="success-message">
+                                Success!
+                            </h5>
+                        :
+                            null
+                    } 
+                    {
+                        isLoading ?
+                            <Bars
+                                height={loadingBarsStyle.height}
+                                width={loadingBarsStyle.width}
+                                color={loadingBarsStyle.color}
+                            />
+                        :
+                            null
+                    }
+                </header>
+                <TextField
+                    id="email-input"
+                    className="form-fields"
+                    label="Email address"
+                    onChange={(e)=>{updateEmailInput(e)}}
+                    sx={textFieldSXProps}
+                />
+                <TextField
+                    className="password-input form-fields"
+                    type="password"
+                    label={!validationError.state ? "Password" : validationError.message}
+                    onChange={(e)=>{updatePassword_1Input(e)}}
+                    sx={textFieldSXProps}
+                />
+                <TextField
+                    className="password-input form-fields"
+                    type="password"
+                    label={!validationError.state ? "Confirm password" : validationError.message}
+                    onChange={(e)=>{updatePassword_2Input(e)}}
+                    sx={textFieldSXProps}
+                />
+                {
+                    validationError.state ? 
+                        <h5>
+                            {validationError.message}
+                        </h5>
+                    :
+                        null
+                }
+                <div id="reset-btn-container">
+                    <button 
+                        className="widget-btn login-reg-submit"
+                        disabled={isLoading ? true : false}
+                        onClick={()=>{submitPasswordUpdate()}}>
+                            {
+                                recoverySuccess ? 
+                                    "Password updated!"
+                                :
+                                    "Submit"
+                            }
+                    </button>
+                    <button 
+                        className="widget-btn login-reg-submit"
+                        onClick={()=>{window.location.href = "/"}}>
+                        Back to the main site
+                    </button>
+                </div>
+            </div>
+        </div>
     )
 }
 
