6dae6bdd8fa0e4347c5e89d83aa17e7c3d09944f
jhersbo <jhersbo@gmail.com>
2023-01-02 09:51:58 -0600

    init sub container

--- src/main_site_components/main_container_components/minor_components/SubscriptionsContainer.tsx
+++ src/main_site_components/main_container_components/minor_components/SubscriptionsContainer.tsx
@@ -1,4 +1,15 @@
+import { useContext } from "react"
+import { BlurContext } from "../../../contexts/global"
+import { CartContext } from "../../../contexts/cart"
+
 const SubscriptionsContainer = ()=>{
+
+    const blurCXT = useContext(BlurContext)
+    let blur = blurCXT?.value
+
+    const cartCXT = useContext(CartContext)
+    let cart = cartCXT.value
+
     return(
         <div id="subscriptions-container">
             Subs container
